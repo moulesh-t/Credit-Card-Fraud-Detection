036be65cc771ece3bece3dfc943351237ad96165
Moulesh T <91364446+moulesh-t@users.noreply.github.com>
2025-10-21 08:35:07 +0530

    Initial Commit

---
+++ src/download.py
@@ -0,0 +1,20 @@
+import os
+import kaggle
+# Downloading Kaggle dataset
+def download():
+    
+    import os
+
+def download():
+    os.makedirs('./data', exist_ok=True)
+    os.system('kaggle datasets download -d mlg-ulb/creditcardfraud -p ./data --unzip')
+    file_path = './data/creditcard.csv'
+    if os.path.exists(file_path):
+        print("Dataset downloaded Successfully")
+    else:
+        print("Error in downloading dataset")
+
+
+
+if __name__ == "__main__":
+    download()
